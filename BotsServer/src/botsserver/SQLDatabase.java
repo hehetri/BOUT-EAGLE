8da630b2c020a1f09f21902489e93a3e7a178804
Lucas Moreira <hehetri@gmail.com>
2026-01-19 16:52:31 -0800

    Add files via upload

---
+++ BotsServer/src/botsserver/SQLDatabase.java
@@ -0,0 +1,143 @@
+package botsserver;
+
+import java.io.*;
+import java.util.*;
+import java.sql.*;
+
+/**
+ *
+ * @author Bouteagle
+ */
+public class SQLDatabase {
+
+    protected String owner;
+    protected Properties sqldata = new Properties();
+    protected Connection con;
+    protected Statement st;
+    protected String ip, port, user, pass, database;
+
+
+    public SQLDatabase(String createdby) {
+        this.owner = createdby;
+    }
+
+
+    protected void debug(String msg) {
+        Main.debug("SQL: "+ msg);
+    }
+
+     /**
+      * Loads the configs out of "configs/mysql.conf"
+      */
+    private void loadconfigs()
+    {
+        try
+        {
+        FileInputStream fin = new FileInputStream("configs/mysql.conf");
+        sqldata.load(fin);
+        fin.close();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        ip = sqldata.getProperty("MySQL_ip");
+        port = sqldata.getProperty("MySQL_port");
+        user = sqldata.getProperty("MySQL_id");
+        pass = sqldata.getProperty("MySQL_pw");
+        database = sqldata.getProperty("MySQL_db");
+    }
+
+    public void start()
+    {
+        loadconfigs();
+
+         /**
+          * setup the basic connection
+          */
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://"+ip+":"+port+"/"+database+"?autoReconnect=true", user,pass);
+            st = con.createStatement();
+            if (!con.equals(null))
+                debug("Connection started successful");
+            //ResultSet rs = st.executeQuery("SELECT * FROM bout_users WHERE username='"+ user +"' LIMIT 1");
+        }
+        catch (Exception ex)
+        {
+            debug("Error : " + ex.getMessage());
+        }
+    }
+    
+    public void recon()
+    {
+    	try{
+    	con.close();
+    	start();
+    	}catch (Exception ex){}
+    }
+
+    public int psupdate(String query, String[] value)
+    {
+		int a=0;
+    	try {
+    		PreparedStatement stmt = null;
+    		stmt = con.prepareStatement(query);
+    		while(a<value.length){
+    			stmt.setString(a+1, value[a]);
+    			a++;
+    		}
+    		stmt.executeUpdate();
+    		stmt.close();
+    		return 2;
+    	}catch (SQLException e){
+        	if(e.getSQLState()==null || e.getSQLState().startsWith("08"))
+        	{
+        		debug("Sql timeout arose" + e);
+        		recon();
+        		return psupdate(query, value);
+        	}
+        	else 
+        		debug("Error (query): " + e.getMessage());
+        }
+        catch (Exception ex)
+        {
+            debug("Error (query): " + ex.getMessage());
+        }
+    	return 1;
+    }
+    
+    public ResultSet psquery(String query, String[] value)
+    {
+		ResultSet rs = null;
+		int a=0;
+    	try {
+    		PreparedStatement stmt = null;
+    		stmt = con.prepareStatement(query);
+    		while(a<value.length){
+    			stmt.setString(a+1, value[a]);
+    			a++;
+    		}
+    		rs=stmt.executeQuery();
+    		stmt.closeOnCompletion();
+    	}catch (SQLException e){
+        	if(e.getSQLState()==null || e.getSQLState().startsWith("08"))
+        	{
+        		debug("Sql timeout arose" + e);
+        		recon();
+        		try{
+            		//rs.close();
+            		}catch(Exception ec){}
+            		rs = psquery(query, value);
+            		return rs;
+        	}
+        	else 
+        		debug("Error (query): " + e.getMessage());
+        }
+        catch (Exception ex)
+        {
+            debug("Error (query): " + ex.getMessage());
+        }
+    	return rs;
+    }
+}
---
+++ BotsServer/start.bat
@@ -0,0 +1,5 @@
+@echo off
+title BotsServer
+cd /d "C:\Users\Administrator\Desktop\source\.____Source\Server\workspace_rar\BotsServer"
+java -jar botsServer.jar
+pause
